4b7dece4f3e250c229fa402d7bf87b2362a3e42c
Ikpia <echukwuka97@gmail.com>
2023-06-09 20:15:39 +0100

    set app.static

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 // express app
 const app = express();
+
+app.static('public');
+app.set('view engine', 'ejs');
 // port number
 const PORT = process.env.PORT || 3000;  
 
