d5e1a06ffadc3071eada427c4187f12ad35339f5
Ikpia <echukwuka97@gmail.com>
2023-06-09 20:19:02 +0100

    view index

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.set('view engine', 'ejs');
 // port number
 const PORT = process.env.PORT || 3000;  
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
