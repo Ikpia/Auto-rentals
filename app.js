03d9ca950ff63c8da88ec76e8a4a5ab84d4c6fdf
Ikpia <echukwuka97@gmail.com>
2023-06-09 22:47:42 +0100

    push changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 const PORT = process.env.PORT || 3000;  
 
 app.get('/', (req, res) => {
-    res.render('login');
+    res.render('index');
 });
 
 app.listen(PORT, () => {
