4f875b84693f9c572cf92d160772dfeeb7313c44
Ikpia <echukwuka97@gmail.com>
2023-06-10 07:33:45 +0100

    push all changes

---
+++ models/signUp.js
@@ -0,0 +1,41 @@
+const signUpSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+
+    phone:{
+        type:Number,
+        require:true
+    },
+
+    email:{
+        type:String,
+        require:true,
+        unique:true,
+        match:/^\S+@\S+\.\S+$/
+    },
+
+    password:{
+        type:String,
+        require:true
+    },
+
+    profession:{
+        type:String,
+        require:true
+    },
+
+    payments:{
+        type:String,
+        default:'not paid'
+    },
+
+},
+{
+    timestamps:true
+}
+);
+
+const SignUp = mongoose.model('SignUp', signUpSchema);
+module.exports = SignUp;
